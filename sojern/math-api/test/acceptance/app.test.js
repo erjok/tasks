41ebe8ba05ea23b5d2ef0135d5e2bc1ce9a5aa40
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2023-07-11 15:27:44 +0100

    First test pass.

--- sojern/math-api/test/acceptance/app.test.js
+++ sojern/math-api/test/acceptance/app.test.js
@@ -4,8 +4,8 @@ should();
 
 describe("app", () => {
   it("should return 200", async () => {
-    const response = await request("http://localhost:3000/").get("/");
-    response.status.should.equal(200)
+    const response = await request("http://localhost:3000").get("/");
+    response.status.should.equal(200);
   });
 });
 
