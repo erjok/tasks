b251c7b500be31b87102681114314e4312c7d0e1
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2023-07-11 12:47:07 +0100

    Equality.

--- sojern/Sojern.Util/VersionComparer.cs
+++ sojern/Sojern.Util/VersionComparer.cs
@@ -2,5 +2,11 @@
 
 public class VersionComparer : IComparer<string>
 {
-    public int Compare(string? version1, string? version2) => 1;
+    public int Compare(string? version1, string? version2)
+    {
+        if (version1 == version2)
+            return 0;
+
+        return 1;
+    }
 }
