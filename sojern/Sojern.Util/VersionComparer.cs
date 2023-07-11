e4064129614bb03ed33060a7ceed96351697e571
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2023-07-11 13:02:36 +0100

    Tests passing.

--- sojern/Sojern.Util/VersionComparer.cs
+++ sojern/Sojern.Util/VersionComparer.cs
@@ -7,6 +7,24 @@ public class VersionComparer : IComparer<string>
         if (version1 == version2)
             return 0;
 
-        return 1;
+        var revisions1 = Array.ConvertAll(version1.Split('.'), Int32.Parse);
+        var revisions2 = Array.ConvertAll(version2.Split('.'), Int32.Parse);
+
+        int i = 0;
+        while(true)
+        {
+            if (revisions1[i] < revisions2[i])
+                return -1;
+
+            if (revisions1[i] > revisions2[i])
+                return 1;
+
+            i++;
+            if (i == revisions1.Length)
+                return -1;
+
+            if (i == revisions2.Length)
+                return 1;
+        }
     }
 }
