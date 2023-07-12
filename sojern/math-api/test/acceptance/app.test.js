668c8de45a4f719f9df39e913d8e505073933927
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2023-07-12 14:32:03 +0100

    Min endpoint test.

--- sojern/math-api/test/acceptance/app.test.js
+++ sojern/math-api/test/acceptance/app.test.js
@@ -2,12 +2,23 @@ import { should } from "chai";
 import request from "supertest";
 should();
 
+const apiUri = "http://localhost:3000";
+
 describe("math api", () => {
-  it("should return version", async () => {
-    const response = await request("http://localhost:3000").get("/");
-    response.status.should.equal(200);
-    response.get('Content-Type').should.match(/json/);
-    response.body.should.have.property("version");
-  });
+    it("should return version", (done) => {
+        request(apiUri)
+            .get("/")
+            .expect(200, {
+                version: '0.1.0'
+            }, done);
+    });
+
+    it("should return min numbers", (done) => {
+        request(apiUri)
+            .get("/min?numbers=3,1,2&q=2")
+            .expect(200, {
+                numbers: [1, 2]
+            }, done);
+    });
 });
 
