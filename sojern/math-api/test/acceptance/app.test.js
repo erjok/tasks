2de2414b7cedd26e3fb245767ba3515595fbb19f
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2023-07-13 12:18:43 +0100

    Quantifier validation test.

--- sojern/math-api/test/acceptance/app.test.js
+++ sojern/math-api/test/acceptance/app.test.js
@@ -56,8 +56,15 @@ describe("math api", () => {
     it("should return bad request when percentile quanitifer is not in the range 0-100", (done) => {
         request(apiUri)
             .get("/percentile?numbers=3,1,2,5&q=101")
+            .expect("Content-Type", /problem\+json/)
             .expect(400, {
-                error: 'Percentile quantifier must be between 0 and 100'
+                title: "One or more validation errors occurred.",
+                status: 400,
+                errors: {
+                    q : [
+                        "Percentile quantifier must be between 0 and 100."
+                    ]
+                }
             }, done);
     });
 });
