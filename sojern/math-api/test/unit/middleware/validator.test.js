855d21e86d4c1a7ec9ee7718835ac32fbdf10049
Sergey Berezovskiy <sergey.berezovskiy@gmail.com>
2023-07-14 17:38:56 +0100

    Request validator tests.

---
+++ sojern/math-api/test/unit/middleware/validator.test.js
@@ -0,0 +1,35 @@
+import chai from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import { check } from 'express-validator';
+
+const should = chai.should();
+chai.use(sinonChai);
+
+describe('request validator middleware' , () => {
+    it('should call next middleware when there are no validation errors', async () => {
+        const req = { query: { foo: 'bar' } };
+        await check('foo').notEmpty().run(req);
+        const next = sinon.spy();
+
+        reqValidator(req, {}, next);
+        next.should.have.been.calledOnceWithExactly();
+    });
+
+    it('should pass the validation error to error-handling middleware when validation error occurs', async () => {
+        const req = { query: { } };
+        await check('foo', 'blah').notEmpty().run(req);
+        const next = sinon.spy();
+
+        reqValidator(req, {}, next);
+
+        const expectedError = sinon.match({
+            code: 'VALIDATION_ERROR',
+            errors: {
+                foo: 'blah'
+            }
+        });
+
+        next.should.have.been.calledOnceWithExactly(expectedError);
+    });
+});
